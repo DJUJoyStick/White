83000a4bc1f85ed8df9d33aa76b6179eb8d839d3
hhh57463 <hhh57463@naver.com>
2022-05-05 18:18:15 +0900

    퍼즐 게임 병합 완료(버그 존재)

---
+++ Assets/2_Game/1_Script/MiniGame/Puzzle/Move.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    public int snapOffset = 50;
+    public GameObject[] piecePos;
+    public Puzzle puzzle;
+    public Transform pieceParent;
+
+    public int piece_no;
+    private int pos_no;
+
+    public Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] str = gameObject.name.Split('_');
+        piece_no = int.Parse(str[1]);
+        pos = transform.localPosition;
+        puzzle.pieceArr.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
+        transform.position = mousePosition;
+        GetComponent<Image>().raycastTarget = false;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        GetComponent<Image>().raycastTarget = true;
+        for (int i = 0; i < piecePos.Length; i++)
+        {
+            if (Vector3.Distance(piecePos[i].transform.position, transform.position) < snapOffset)
+            {
+                pos_no = piecePos[i].GetComponent<PosSet>().pos_no;
+                transform.SetParent(piecePos[i].transform);
+                transform.localPosition = Vector3.zero;
+                puzzle.CheckedPuzzle(pos_no - 1, piece_no);
+            }
+        }
+    }
+    public void RePlace()
+    {
+        transform.SetParent(pieceParent);
+        transform.localPosition = pos;
+    }
+}
